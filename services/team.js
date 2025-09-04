d97cc1ac6a49ebdb3e1d182f3b2804f32d216cf7
Debarun <paldebarun27@gmail.com>
2025-09-04 11:34:07 +0530

    modified the teams getAllMembers function to not send the data in a sorted order

--- services/team.js
+++ services/team.js
@@ -1,32 +1,6 @@
 const Team = require("../models/team");
 const {uploadImageFile} =require('./image_upload')
 
-// Create a new team member
-// exports.createTeamMember = async (req, res) => {
-//   try {
-//     let imageUrl = "";
-//     console.log("This is file : ",req.files)
-//     if (req.files && req.files.imageFile) {
-//       console.log("sending file : ",req.files.imageFile)
-//       const uploadResponse = await imageUpload(req, res);
-//       if (uploadResponse.success) {
-//         imageUrl = uploadResponse.imageUrl;
-//       } 
-//       else {
-//         return res.status(400).json({ success: false, message: uploadResponse.message });
-//       }
-//     }
-
-//     const data = { ...req.body, image_url: imageUrl };
-//     const teamMember = new Team(data);
-//     await teamMember.save();
-
-//     return res.status(201).json({ success: true, teamMember });
-//   } catch (error) {
-//     return res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
 exports.createTeamMember = async (req, res) => {
   try {
     let imageUrl = "";
@@ -48,7 +22,7 @@ exports.createTeamMember = async (req, res) => {
 
 exports.getAllTeamMembers = async (req, res) => {
   try {
-    const teamMembers = await Team.find().populate("expertise").sort({ createdAt: -1 }); // newest first
+    const teamMembers = await Team.find().populate("expertise"); // newest first
     return res.status(200).json({ success: true, teamMembers });
   } catch (error) {
     return res.status(500).json({ success: false, message: "Error fetching team members: " + error.message });
